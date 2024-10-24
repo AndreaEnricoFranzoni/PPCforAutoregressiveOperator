4c935c71108978a2894837e7750e9f952b30bfd4
Andrea Enrico Franzoni <andreaenrico.franzoni@gmail.com>
2024-10-24 20:34:42 +0200

    Corrected error

--- src/strategy_cv_imp.hpp
+++ src/strategy_cv_imp.hpp
@@ -1,86 +1,45 @@
-#ifndef KO_REMOVE_NAN_HPP
-#define KO_REMOVE_NAN_HPP
-
-#include <Eigen/Dense>
-#include <iostream>
-#include <algorithm>
-#include <iterator>
-#include <numeric>
-#include <functional>
-#include <vector>
-#include <set>
-#include <type_traits>
+#include "strategy_cv.hpp"
 
 
-#include "parameters_wrapper.hpp"
-#include "traits_ko.hpp"
-
-
-
-//to do tag dispatching for the correct way of removing nans
-template <REM_NAN MA_t>
-using MAT = std::integral_constant<REM_NAN, MA_t>;
-
-
-template<typename T,REM_NAN MA_t>
-class removing_nan
+//Augmenting window strategy division in train and validation
+template<CV_STRAT cv_strat>
+void
+cv_strategy<cv_strat>::train_validation_set_strategy(int n,CV_STRAT_T<CV_STRAT::AUGMENTING_WINDOW>)
 {
   
-private:
-  KO_Traits::StoringMatrix m_data;
-  std::size_t m_m;
-  std::size_t m_n;
-  std::set<int> m_row_to_be_removed;
-  std::size_t m_first_ti;
-  std::size_t m_last_ti;
-  std::vector<double> m_weights;
+  //defining the augmenting moving window for the train and validation sets 
+  //how many, from the start, contiguos time instants: taking the ceil of half of the time instants as the smallest one
+  int min_dim_ts = static_cast<int>(std::ceil(static_cast<double>(n)/static_cast<double>(2)));
+  //the biggest is all the time instants but one
+  int max_dim_ts = n;
   
-  
-  void row_removal(Eigen::Block<Eigen::Matrix<T,-1,-1>,1>& row, MAT<REM_NAN::MR>);
-  void row_removal(Eigen::Block<Eigen::Matrix<T,-1,-1>,1>& row, MAT<REM_NAN::ZR>);
-  
-public:
-  
-  //constructor: takes the matrix from which there is need to leave out the nans
-  template<typename STOR_OBJ>
-  removing_nan(STOR_OBJ&& data)
-    :
-    m_data{std::forward<STOR_OBJ>(data)}
-    { 
-      m_m = m_data.rows();
-      m_n = m_data.cols();
-    }
-  
-  //getter for m_data
-  inline KO_Traits::StoringMatrix data() const {return m_data;};
-  
-  //removing rows with too many nans
-  KO_Traits::StoringMatrix final_clean() const;
-  
-  //how many nans to actually remove an entire row?
-  inline bool
-  const
-  cond_rem_row(Eigen::Block<Eigen::Matrix<T,-1,-1>,1>& row)
+  //train set is: from the beginning up to a time instant
+  //validation set is: the next time instant
+  m_strategy.reserve(max_dim_ts - min_dim_ts);
+  for(std::size_t i = static_cast<std::size_t>(min_dim_ts); i < static_cast<std::size_t>(max_dim_ts); ++i)
   { 
-    //condition for removing a row since there are too many nans: TODO: change this?
-    //all nans in the row
-    return std::count_if(row.begin(),row.end(),[](T el){return isnan(el);}) == m_n  ?  true  :  false;
-  };
-  
-  //how to leave out the nans in each row: tag dispatching is performed to substitute them correctly
-  void row_removal(Eigen::Block<Eigen::Matrix<T,-1,-1>,1>& row) { return row_removal(row, MAT<MA_t>{});};
-  
-  //for each row: if not too many nans: substitute them; else, the row has to be removed
-  inline void remove_nan()
-  { int counter_row = 1;
-    for(auto row : m_data.rowwise()){ if(cond_rem_row(row)){m_row_to_be_removed.insert(counter_row);} else{row_removal(row);};   counter_row++;} 
-    if(!m_row_to_be_removed.empty()){ m_data = final_clean();}   
-  };
-  
-};
+    //train set: i: Eigen takes the first p left cols starting from 1
+    std::vector<int> train_set;
+    train_set.reserve(1);
+    train_set.emplace_back(i);
+    
+    //validation set: i: Eigen takes the i-th column starting from 0
+    std::vector<int> validation_set;
+    validation_set.reserve(1);
+    validation_set.emplace_back(i);
+    
+    m_strategy.emplace_back(std::make_pair(train_set,validation_set));
+  }
+}
 
 
-#include "removing_nan_imp.hpp"
-#include "removing_nan_cleaner_imp.hpp"
 
-#endif /*KO_REMOVE_NAN_HPP*/
+//Augmenting window strategy test and validation set
+//the first element of the pair is the test set, the second one is validation set
+template<CV_STRAT cv_strat>
+train_valid_set_t
+cv_strategy<cv_strat>::train_validation_set(const KO_Traits::StoringMatrix &data, const iter_cv_t &strat, CV_STRAT_T<CV_STRAT::AUGMENTING_WINDOW>)
+const
+{
+  return std::make_pair( data.leftCols(strat.first.front()), data.col(strat.second.front()) );
+}
