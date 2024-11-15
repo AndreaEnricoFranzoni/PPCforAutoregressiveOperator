b6c4ff6f8f4ef910f7879bc2165dc4ee1a68c028
Andrea Enrico Franzoni <andreaenrico.franzoni@gmail.com>
2024-11-15 13:29:47 +0100

    Updated export

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -12,7 +12,7 @@ Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
 #endif
 
 // PPC_KO
-Rcpp::List PPC_KO(Rcpp::NumericMatrix X, std::string id_CV, double alpha, int k, double threshold_ppc, Rcpp::Nullable<NumericVector> alpha_vec, Rcpp::Nullable<IntegerVector> k_vec, double toll, Rcpp::Nullable<NumericVector> disc_ev, double left_extreme, double right_extreme, int min_size_ts, int max_size_ts, int err_ret, Rcpp::Nullable<std::string> id_rem_nan);
+Rcpp::List PPC_KO(Rcpp::NumericMatrix X, std::string id_CV, double alpha, int k, double threshold_ppc, Rcpp::Nullable<NumericVector> alpha_vec, Rcpp::Nullable<IntegerVector> k_vec, double toll, Rcpp::Nullable<NumericVector> disc_ev, double left_extreme, double right_extreme, Rcpp::Nullable<int> min_size_ts, Rcpp::Nullable<int> max_size_ts, int err_ret, Rcpp::Nullable<std::string> id_rem_nan);
 RcppExport SEXP _PPCKO_PPC_KO(SEXP XSEXP, SEXP id_CVSEXP, SEXP alphaSEXP, SEXP kSEXP, SEXP threshold_ppcSEXP, SEXP alpha_vecSEXP, SEXP k_vecSEXP, SEXP tollSEXP, SEXP disc_evSEXP, SEXP left_extremeSEXP, SEXP right_extremeSEXP, SEXP min_size_tsSEXP, SEXP max_size_tsSEXP, SEXP err_retSEXP, SEXP id_rem_nanSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
@@ -28,8 +28,8 @@ BEGIN_RCPP
     Rcpp::traits::input_parameter< Rcpp::Nullable<NumericVector> >::type disc_ev(disc_evSEXP);
     Rcpp::traits::input_parameter< double >::type left_extreme(left_extremeSEXP);
     Rcpp::traits::input_parameter< double >::type right_extreme(right_extremeSEXP);
-    Rcpp::traits::input_parameter< int >::type min_size_ts(min_size_tsSEXP);
-    Rcpp::traits::input_parameter< int >::type max_size_ts(max_size_tsSEXP);
+    Rcpp::traits::input_parameter< Rcpp::Nullable<int> >::type min_size_ts(min_size_tsSEXP);
+    Rcpp::traits::input_parameter< Rcpp::Nullable<int> >::type max_size_ts(max_size_tsSEXP);
     Rcpp::traits::input_parameter< int >::type err_ret(err_retSEXP);
     Rcpp::traits::input_parameter< Rcpp::Nullable<std::string> >::type id_rem_nan(id_rem_nanSEXP);
     rcpp_result_gen = Rcpp::wrap(PPC_KO(X, id_CV, alpha, k, threshold_ppc, alpha_vec, k_vec, toll, disc_ev, left_extreme, right_extreme, min_size_ts, max_size_ts, err_ret, id_rem_nan));
@@ -37,7 +37,7 @@ BEGIN_RCPP
 END_RCPP
 }
 // PPC_KO_2d
-Rcpp::List PPC_KO_2d(Rcpp::NumericMatrix X, std::string id_CV, double alpha, int k, double threshold_ppc, Rcpp::Nullable<NumericVector> alpha_vec, Rcpp::Nullable<IntegerVector> k_vec, double toll, Rcpp::Nullable<NumericVector> disc_ev_x1, int num_disc_ev_x1, Rcpp::Nullable<NumericVector> disc_ev_x2, int num_disc_ev_x2, double left_extreme_x1, double right_extreme_x1, double left_extreme_x2, double right_extreme_x2, int min_size_ts, int max_size_ts, int err_ret, Rcpp::Nullable<std::string> id_rem_nan);
+Rcpp::List PPC_KO_2d(Rcpp::NumericMatrix X, std::string id_CV, double alpha, int k, double threshold_ppc, Rcpp::Nullable<NumericVector> alpha_vec, Rcpp::Nullable<IntegerVector> k_vec, double toll, Rcpp::Nullable<NumericVector> disc_ev_x1, int num_disc_ev_x1, Rcpp::Nullable<NumericVector> disc_ev_x2, int num_disc_ev_x2, double left_extreme_x1, double right_extreme_x1, double left_extreme_x2, double right_extreme_x2, Rcpp::Nullable<int> min_size_ts, Rcpp::Nullable<int> max_size_ts, int err_ret, Rcpp::Nullable<std::string> id_rem_nan);
 RcppExport SEXP _PPCKO_PPC_KO_2d(SEXP XSEXP, SEXP id_CVSEXP, SEXP alphaSEXP, SEXP kSEXP, SEXP threshold_ppcSEXP, SEXP alpha_vecSEXP, SEXP k_vecSEXP, SEXP tollSEXP, SEXP disc_ev_x1SEXP, SEXP num_disc_ev_x1SEXP, SEXP disc_ev_x2SEXP, SEXP num_disc_ev_x2SEXP, SEXP left_extreme_x1SEXP, SEXP right_extreme_x1SEXP, SEXP left_extreme_x2SEXP, SEXP right_extreme_x2SEXP, SEXP min_size_tsSEXP, SEXP max_size_tsSEXP, SEXP err_retSEXP, SEXP id_rem_nanSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
@@ -58,8 +58,8 @@ BEGIN_RCPP
     Rcpp::traits::input_parameter< double >::type right_extreme_x1(right_extreme_x1SEXP);
     Rcpp::traits::input_parameter< double >::type left_extreme_x2(left_extreme_x2SEXP);
     Rcpp::traits::input_parameter< double >::type right_extreme_x2(right_extreme_x2SEXP);
-    Rcpp::traits::input_parameter< int >::type min_size_ts(min_size_tsSEXP);
-    Rcpp::traits::input_parameter< int >::type max_size_ts(max_size_tsSEXP);
+    Rcpp::traits::input_parameter< Rcpp::Nullable<int> >::type min_size_ts(min_size_tsSEXP);
+    Rcpp::traits::input_parameter< Rcpp::Nullable<int> >::type max_size_ts(max_size_tsSEXP);
     Rcpp::traits::input_parameter< int >::type err_ret(err_retSEXP);
     Rcpp::traits::input_parameter< Rcpp::Nullable<std::string> >::type id_rem_nan(id_rem_nanSEXP);
     rcpp_result_gen = Rcpp::wrap(PPC_KO_2d(X, id_CV, alpha, k, threshold_ppc, alpha_vec, k_vec, toll, disc_ev_x1, num_disc_ev_x1, disc_ev_x2, num_disc_ev_x2, left_extreme_x1, right_extreme_x1, left_extreme_x2, right_extreme_x2, min_size_ts, max_size_ts, err_ret, id_rem_nan));
