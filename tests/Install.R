13750ce6dfada1fa9c808135e8ae67a47ad16b10
Andrea Enrico Franzoni <andreaenrico.franzoni@gmail.com>
2024-11-16 09:18:51 +0100

    Cleaning package

--- tests/Install.R
+++
@@ -1,9 +0,0 @@
-#to uploda the packages
-#change here the directory
-#setwd("/Users/andreafranzoni/Documents/Politecnico/Magistrale/PACS/pacs-project/PPCforAutoregressiveOperator")
-#then 
-#Rcpp::compileAttributes(".") 
-# and then push the changes to modify the exports
-
-library(devtools)
-devtools::install_github("AndreaEnricoFranzoni/PPCforAutoregressiveOperator", force = TRUE)
