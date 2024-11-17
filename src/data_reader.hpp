85bd89017558377c65049c894272bbba1fd00658
Andrea Enrico Franzoni <145605679+AndreaEnricoFranzoni@users.noreply.github.com>
2024-11-17 14:47:10 +0100

    Update data_reader.hpp

--- src/data_reader.hpp
+++ src/data_reader.hpp
@@ -25,8 +25,7 @@ std::pair<KO_Traits::StoringMatrix,std::vector<int>>
 reader_data(Rcpp::NumericMatrix X,
             REM_NAN MA_t)
 {
-  std::cout << "Remote version 1.0.0" << std::endl;
-
+  
   //taking the dimensions: n_row is the number of time series, n_col is the number of time istants
   int n_row = X.nrow();
   int n_col = X.ncol();
