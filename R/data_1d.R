bc8ee63e795b50a1b1927108262af425e21bc1d9
Andrea Enrico Franzoni <andreaenrico.franzoni@gmail.com>
2024-11-17 16:07:14 +0100

    Data 1d

---
+++ R/data_1d.R
@@ -0,0 +1,14 @@
+#' Example Dataset: my_data
+#'
+#' A simple example dataset included in the package.
+#'
+#' @format A data frame with 10 rows and 2 variables:
+#' \describe{
+#'   \item{x}{a numeric vector (1:10)}
+#'   \item{y}{a numeric vector with random values}
+#' }
+#' @usage data(my_data)
+#' @examples
+#' data(my_data)
+#' head(my_data)
+"data_1d"
