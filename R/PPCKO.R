65382250ecdcbb7be4a94e0fa688afacdb5bbd77
Andrea Enrico Franzoni <andreaenrico.franzoni@gmail.com>
2024-12-15 17:38:18 +0100

    Mod doc

--- R/PPCKO.R
+++ R/PPCKO.R
@@ -27,37 +27,40 @@
 #'@examples
 #' library(PPCKO)
 #' ############# CURVE CASE ##########################
-#' 
-#' # upload data
+#' \dontrun{
+#' ### upload data
 #' data(data_1d)
-#' # check hp
+#' ### check hp
 #' check_hp_1d <- KO_check_hps(data_1d)
-#' # forecasting algorithm
+#' ### forecasting algorithm
 #' res_1d <- PPC_KO( X = data_1d,
 #'                   id_CV = "NoCV",
 #'                   alpha = 0.1,
 #'                   k = 3)
-#' # visualize results
+#' ### visualize results
 #' KO_show_results(res_1d,check_hp_1d)
+#' }
 #'
 #'
 #'
 #' ############# SURFACE CASE ##########################
-#' 
-#' # upload data
+#' \dontrun{
+#' ### upload data
 #' data(data_2d)
+#' ### wrap them to have a correct input
 #' data_2d_wrap = data_2d_wrapper_from_list(data_2d)
-#' # check hp
+#' ### check hp
 #' check_hp_2d <- KO_check_hps_2d(data_2d_wrap,dim_x1=10,dim_x2=10)
-#' # forecasting algorithm
+#' ### forecasting algorithm
 #' res_2d <- PPC_KO_2d( X = data_2d_wrap,
 #'                      id_CV = "NoCV",
 #'                      alpha = 0.1,
 #'                      k = 3,
 #'                      num_disc_ev_x1 = 10,
 #'                      num_disc_ev_x2 = 10)
-#' # visualize results
+#' ### visualize results
 #' KO_show_results_2d(res_2d,check_hp_2d)
+#' }
 #'
 #'@docType package
 #'@name PPCKO
