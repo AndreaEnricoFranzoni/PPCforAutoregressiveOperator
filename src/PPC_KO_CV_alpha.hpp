709727ede8cbb82174a66f951598eed7a61818b4
Andrea Enrico Franzoni <145605679+AndreaEnricoFranzoni@users.noreply.github.com>
2025-01-03 19:53:58 +0100

    print check

--- src/PPC_KO_CV_alpha.hpp
+++ src/PPC_KO_CV_alpha.hpp
@@ -28,6 +28,7 @@ public:
     m_max_size_ts(max_size_ts)
     {
       this->k() = k; 
+      std::cout << "Constructor cv alpha k: " << this->k() << std::endl;
       
 #ifdef _OPENMP
 #pragma omp parallel for num_threads(this->number_threads())
@@ -49,7 +50,7 @@ public:
     m_max_size_ts(max_size_ts)
     {
       this->threshold_ppc() = threshold_ppc; 
-      std::cout << "Constructor: " << this->threshold_ppc() << std::endl;
+      std::cout << "Constructor cv alpha thresh: " << this->threshold_ppc() << std::endl;
       
 #ifdef _OPENMP
 #pragma omp parallel for num_threads(this->number_threads())
